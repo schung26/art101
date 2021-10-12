c505aa8a526e50db793e8b26e9f81545529bdb23
schung26 <schung26@ucsc.edu>
2021-10-12 15:12:16 -0700

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ //Summary: This is for lab4 javascript.
+ //Description.These comments are for the game hangman.
+ // @link   URL
+ // @file   Hangman.
+ // @author Leola and Sharon.
+ // @since  10.12.2021
+ */
+ //Set up a board
+      //print vertical line
+        //print shorter horizontal line from vertical line
+  //Decide players and order
+    // determine between player and computer
+  //Randomize words selection
+    //get word from wordbank
+  //Player selects letter
+    //input letter from 26 options
+        //if letter input is correct then input letter
+        //if letter input is incorrent then body part is displayed.
+  //Repeat until all body parts are displayed
+    // or until the word is filled
