efe79841959362b0696b8d780286c37f368cee71
schung26 <schung26@ucsc.edu>
2021-10-18 12:11:56 -0700

    Lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/*
+ * Author:  Sharon Chung and Leola Sanchez
+ * Created:   10.18.2021
+ * License: Public Domain.
+ */
+
+// Define Variables
+ make = "Kia";
+ model = "Soul";
+ color = "Gray";
+ year = "2013";
+
+// calculate
+
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
